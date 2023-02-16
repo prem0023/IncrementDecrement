4ebf8a0599a81cca61e6b92109e09a9febea88ab
prem023 <phanishwarnath2@gmail.com>
2023-02-17 00:50:48 +0530

    Prjoect build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
 
 function App() {
+  const [num, setNum] = useState(0);
+
+  const increment = () => {
+    setNum(num + 1);
+  }
+
+  const decrement = () => {
+    if(num === 0){
+      alert('Sorry, Zero Limit Reached');
+      return;
+    }
+    setNum(num - 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='main_div'>
+        <div className='center_div'>
+          <h1> {num} </h1>
+          <div className='btn_div'>
+            <button type='submit' onClick={increment} > Increm</button>
+            <button type='submit' onClick={decrement} > Decrem</button>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
